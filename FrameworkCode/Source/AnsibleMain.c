769607efa66e1eb37373dd3e0157a68a575dda0d
WIN\skoppaka <skoppaka@stanford.edu>
2018-05-10 23:47:26 -0700

    Set up UART on the TIVA for XBee

--- FrameworkCode/Source/AnsibleMain.c
+++ FrameworkCode/Source/AnsibleMain.c
@@ -21,9 +21,37 @@ first pass      s koppaka
 */
 #include "ES_Configure.h"
 #include "ES_Framework.h"
-#include "TemplateFSM.h"
+
+
+#include "ES_Framework.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_sysctl.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/pin_map.h"  
+#include "driverlib/gpio.h"
+
+
+#include "inc/hw_pwm.h"
+#include "inc/hw_nvic.h"
+#include "inc/hw_timer.h"
+#include "inc/tm4c123gh6pm.h" // Define PART_TM4C123GH6PM in project
+#include "inc/hw_uart.h" 
+
+
+#include "AnsibleMain.h"
 
 /*----------------------------- Module Defines ----------------------------*/
+#define BitsPerNibble 4
+#define UART2_RX_PIN GPIO_PIN_6
+#define UART2_TX_PIN GPIO_PIN_7
 
 /*---------------------------- Module Functions ---------------------------*/
 /* prototypes for private functions for this machine.They should be functions
@@ -57,11 +85,14 @@ static uint8_t MyPriority;
  Author
      J. Edward Carryer, 10/23/11, 18:55
 ****************************************************************************/
-bool InitTemplateFSM(uint8_t Priority)
+bool InitAnsibleMain(uint8_t Priority)
 {
   ES_Event_t ThisEvent;
 
   MyPriority = Priority;
+  
+  UARTHardwareInit(); //Initialize HW for UART 
+  
   // put us into the Initial PseudoState
   CurrentState = InitPState;
   // post the initial transition event
@@ -186,3 +217,54 @@ TemplateState_t QueryTemplateFSM(void)
  private functions
  ***************************************************************************/
 
+
+
+/***************************************************************************
+ public functions
+ ***************************************************************************/
+static void UARTHardwareInit(void){
+//Setting up the registers for UART-XBee communications
+  
+  //Enable the clock to the UART module using the RCGCUART (run time gating clock control) register
+   HWREG(SYSCTL_RCGCUART) |= SYSCTL_RCGCUART_R2; //UART2 Clock
+  
+  //Wait for the UART to be ready (PRUART)
+   while ((HWREG(SYSCTL_PRUART) & SYSCTL_PRUART_R2) != SYSCTL_PRUART_R2); 
+  
+  //Enable the clock to the GPIO port D
+   HWREG(SYSCTL_RCGCGPIO) |= SYSCTL_RCGCGPIO_R3;
+  
+  //Wait for the GPIO module to be ready  (PRGPIO)
+   while ((HWREG(SYSCTL_PRGPIO) & SYSCTL_RCGCGPIO_R3) != SYSCTL_RCGCGPIO_R3); 
+  
+  //Configure the GPIO pine for in/out/drive-level/drive-type 
+     HWREG(GPIO_PORTD_BASE+GPIO_O_DEN) |= (UART2_TX_PIN|UART2_RX_PIN); //setting pins as digital
+     HWREG(GPIO_PORTD_BASE+GPIO_O_DIR) &= ~(UART2_RX_PIN); //setting RX as input 
+     HWREG(GPIO_PORTD_BASE+GPIO_O_DIR) |= (UART2_RX_PIN); //setting TX as output
+  
+  //Select the Alternative functions for the UART pins (AFSEL)(AFSEL Table pg.1351)
+   HWREG(GPIO_PORTD_BASE+GPIO_O_AFSEL) |= (UART2_TX_PIN|UART2_RX_PIN); 
+    
+  //Configure the PMCn fields in the GPPIOPCTL (p.689) register to assign the UART pins
+    HWREG(GPIO_PORTD_BASE+GPIO_O_PCTL) = (HWREG(GPIO_PORTD_BASE+GPIO_O_PCTL) & 0xfffffff0)+(1); //Write 1 to select U2RX as alternative function 
+    HWREG(GPIO_PORTD_BASE+GPIO_O_PCTL) = (HWREG(GPIO_PORTD_BASE+GPIO_O_PCTL) & 0xffffff0f)+(1<<(1* BitsPerNibble)) ; //Write 1 to select U2TX as alt fun
+
+  //Disable the UART by clearning the UARTEN bits in the UARTCTL register
+    //UART Data Registers are cleared by default (RXE and TXE are already enabled) **
+  
+  //Write the inter portion of the URTIBRD register (setting baud rate) 
+    HWREG(UART2_BASE+UART_O_IBRD) = HWREG(UART2_BASE+UART_O_IBRD) + 0x15;  //writing 21 in hex
+  
+  //Write the fractional portion of the BRD to the UARTIBRD register
+    HWREG(UART2_BASE+UART_O_FBRD) = HWREG(UART2_BASE+UART_O_FBRD) + 0x2D;  //writing 21 in hex; //writing 45 in hex 
+
+  //Write the desired serial parameters to the UARTLCRH registers to set word length to 8
+     HWREG (UART2_BASE + UART_O_LCRH) = HWREG(UART2_BASE + UART_O_LCRH) + (UART_LCRH_WLEN_8); 
+
+  //Configure the UART operation using the UARTCTL register 
+     //UART Data Registers are cleared by default (RXE and TXE are already enabled) **
+  
+  //Enable the UART by setting the UARTEN bit in the UARTCTL register 
+    HWREG(UART2_BASE + UART_O_IM) |= (UART_CTL_UARTEN); 
+        
+  }
